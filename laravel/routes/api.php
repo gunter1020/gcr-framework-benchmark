01e1d56d38651501404eb6e87bbf9a65011318b3
Gunter Chou <abcd2221925@gmail.com>
2023-11-12 23:28:57 +0800

    feat(laravel): Add test controller

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/ping', fn (Request $request) => [
+    'status' => 'pong',
+    'server' => [
+        'time' => Carbon::now()->format('Y-m-d H:i:s'),
+    ],
+    'user' => [
+        'ip' => $request->ip(),
+        'agent' => $request->userAgent(),
+    ],
+]);
